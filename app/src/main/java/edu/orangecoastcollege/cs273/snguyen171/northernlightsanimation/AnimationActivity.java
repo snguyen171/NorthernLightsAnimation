49be8f4ae2ba104d6ffb0ba5d62d6319174d02ca
snguyen171 <sonngn4@gmail.com>
2016-10-04 16:39:55 -0700

    80% done more work to do

--- app/src/main/java/edu/orangecoastcollege/cs273/snguyen171/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/snguyen171/northernlightsanimation/AnimationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class AnimationActivity extends AppCompatActivity {
@@ -34,5 +35,27 @@ public class AnimationActivity extends AppCompatActivity {
             frameAnim.stop();
         else
             frameAnim.start();
+    }
+
+    public void toggleRotateAnim(View view)
+    {
+        if (rotateAnim != null && rotateAnim.hasStarted())
+        {
+            lightsImageView.clearAnimation();
+            rotateAnim = null;
+        }
+        else
+        {
+            rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+            lightsImageView.startAnimation(rotateAnim);
+
+        }
+
+    }
+
+    public void toggleShakeAnim(View view)
+    {
+        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+        lightsImageView.startAnimation(shakeAnim);
     }
 }
